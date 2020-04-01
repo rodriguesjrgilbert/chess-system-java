d9a81432995f292c3cb55ac51435c89a42929b0d
Gilberto Junior Rodrigues <gilberto.junior@outlook.com>
2020-04-01 11:43:43 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardExecption.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class BoardExecption extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public BoardExecption(String msg) {
+		super(msg);
+	}
+}
